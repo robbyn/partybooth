9ae6c4ca8ede9a68a0d11bb2da175488de9b4bb6
maurice@perry.ch <maurice@perry.ch@5ab6e794-5428-e253-ca8a-3050303bc5eb>
2012-04-11 13:43:03 +0000

    Check upload integrity digest, and use java.util.logging instead of System.out

---
+++ partybooth/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Arduino-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/Arduino-MacOSX
+CND_ARTIFACT_NAME_Debug=partybooth.elf
+CND_ARTIFACT_PATH_Debug=dist/Debug/Arduino-MacOSX/partybooth.elf
+CND_PACKAGE_DIR_Debug=dist/Debug/Arduino-MacOSX/package
+CND_PACKAGE_NAME_Debug=partybooth.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Arduino-MacOSX/package/partybooth.tar
+# Release configuration
+CND_PLATFORM_Release=Arduino-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/Arduino-MacOSX
+CND_ARTIFACT_NAME_Release=partybooth.elf
+CND_ARTIFACT_PATH_Release=dist/Release/Arduino-MacOSX/partybooth.elf
+CND_PACKAGE_DIR_Release=dist/Release/Arduino-MacOSX/package
+CND_PACKAGE_NAME_Release=partybooth.tar
+CND_PACKAGE_PATH_Release=dist/Release/Arduino-MacOSX/package/partybooth.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
