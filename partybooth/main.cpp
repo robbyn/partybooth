c60d549265ee6cf3525db93d9ef2fadbccfe3c61
maurice@perry.ch <maurice@perry.ch@5ab6e794-5428-e253-ca8a-3050303bc5eb>
2012-04-14 20:26:53 +0000

    Check upload integrity digest, and use java.util.logging instead of System.out

--- partybooth/main.cpp
+++ partybooth/main.cpp
@@ -1,19 +1,29 @@
 #include "common.h"
 #include <avr/io.h>
 #include <avr/sleep.h>
+#include <avr/interrupt.h>
 
 static const uint8_t LED = PB0;
 static const uint8_t BTN = PB1;
 
+static uint16_t counter = 0;
+
+ISR(TIM0_COMPA_vect) {
+    OCR0A ^= 1;
+    if (++counter >= 4000) {
+        // every second, toggle LED
+        PORTB ^= ~(1 << LED);
+    }
+}
+
 int main() {
-    set_sleep_mode(SLEEP_MODE_IDLE);   // set sleep mode 
-    DDRB |= 1 << LED; // LED output
-    DDRB &= ~(1 << BTN); // BTN input
-    while (1) {
-        if (PINB & (1 << BTN)) {
-            PORTB &= ~(1 << LED);
-        } else {
-            PORTB |= 1 << LED;
-        }
+    TCCR0B = 2; // top = OCRA
+    TCCR0B = 2; // clk/8 prescale
+    OCR0A = 37;
+    TCNT0 = 0;
+    TIMSK0 = 4; // Enable interrupt for compare match A
+    sei();
+    while (true) {
+        sleep_mode();
     }
 }
