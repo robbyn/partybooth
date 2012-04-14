e67c7c57d550535bd538b1736da936e984f8252a
maurice@perry.ch <maurice@perry.ch@5ab6e794-5428-e253-ca8a-3050303bc5eb>
2012-04-14 18:44:26 +0000

    Check upload integrity digest, and use java.util.logging instead of System.out

--- trunk/partybooth/main.cpp
+++ trunk/partybooth/main.cpp
@@ -1,9 +1,11 @@
 #include <avr/io.h>
+#include <avr/sleep.h>
 
 static const uint8_t LED = PB0;
 static const uint8_t BTN = PB1;
 
 int main() {
+    set_sleep_mode(SLEEP_MODE_IDLE);   // set sleep mode 
     DDRB |= 1 << LED; // LED output
     DDRB &= ~(1 << BTN); // BTN input
     while (1) {
