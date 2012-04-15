0d174c5cc2f3387079b1fb651552bf32871989fa
maurice@perry.ch <maurice@perry.ch@5ab6e794-5428-e253-ca8a-3050303bc5eb>
2012-04-15 07:40:47 +0000

    Check upload integrity digest, and use java.util.logging instead of System.out

--- trunk/partybooth/main.cpp
+++ trunk/partybooth/main.cpp
@@ -6,26 +6,75 @@
 static const uint8_t LED = PB0;
 static const uint8_t BTN = PB1;
 static const uint8_t BZZ = PB2;
+static const uint8_t SHT = PB3;
 
+static uint8_t state = 0;
 static uint16_t counter = 0;
 
 ISR(TIM0_COMPA_vect) {
     OCR0A ^= 1;
-    if (++counter >= 4000) {
-        counter = 0;
-        // every second, toggle LED
-        PORTB ^= 1 << LED;
+    ++counter;
+    switch (state) {
+        case 2:
+        case 4:
+        case 6:
+        case 8:
+            if (counter >= 3200) {
+                counter = 0;
+                ++state;
+                PORTB |= 1 << LED;
+            }
+            break;
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+            if (counter >= 800) {
+                counter = 0;
+                ++state;
+                PORTB &= ~((1 << LED) | (1 << BZZ));
+            } else {
+                PORTB ^= 1 << BZZ;
+            }
+            break;
+        case 9:
+            if (counter >= 4000) {
+                counter = 0;
+                ++state;
+                PORTB &= ~((1 << LED) | (1 << BZZ));
+                PORTB |= 1 << SHT;
+            } else {
+                PORTB ^= 1 << BZZ;
+            }
+            break;
+        case 10:
+            if (counter >= 800) {
+                counter = 0;
+                state = 0;
+                PORTB &= ~(1 << SHT);
+            }
+            break;
     }
 }
 
+ISR(INT0_vect) {
+    counter = 0;
+    state = 1;
+    PORTB &= ~(1 << SHT);
+    PORTB |= 1 << LED;
+}
+
 int main() {
-    DDRB |= (1 << LED) | (1 << BZZ); // LED output
-    PORTB &= ~(1 << LED);
+    DDRB |= (1 << LED) | (1 << BZZ) | (1 << BZZ); // LED, BZZ, SHT output
+    // Setup timer 0 for a frequency of 4kHz
     TCCR0A = 2; // top = OCRA
-    TCCR0B = 2; // clk/8 prescale
-    OCR0A = 37;
+    TCCR0B = 2; // /8 prescale in addition to the /8 system clock prescale
+    OCR0A = 37; // alternate between 36 and 37 (the correct count is 36.5)
     TCNT0 = 0;
     TIMSK0 = 4; // Enable interrupt for compare match A
+    // Setup BTN interrupt
+    MCUCR |= 3; // interrupt on the rising edge
+    GIMSK |= 1 << INT0; // enable interrupt from INT0
     sei();
     while (true) {
         sleep_mode();
